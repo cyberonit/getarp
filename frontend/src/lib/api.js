4a024e10d72f6525fc6a494050251c33197bc02b
Stefan Ionita <stefan.ionita@gmail.com>
2026-06-15 22:22:11 +0000

    Remove Bouncer Manager and Cowrie Honeypot panels from Settings tab

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -38,9 +38,7 @@ export const api = {
   docs: () => get('/docs'),
   docUrl: (name) => `${BASE}/docs/${name}`,
 
-  crowdsecDecisions: () => get('/admin/crowdsec/decisions'),
   crowdsecOverview: () => get('/admin/crowdsec/overview'),
-  cowrieSessions: () => get('/admin/cowrie/sessions'),
 
   settings: () => get('/admin/settings'),
   async saveSetting(key, value) {
